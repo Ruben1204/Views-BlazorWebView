94f023aed3fd0bfcfc43ea90cef4b1c7e3e59f5f
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2023-04-25 14:39:23 -0500

    Fisrt commit
    Add control BlazorWebView in project blazor hybrid

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TutorialesMauiBlazor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TutorialesMauiBlazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
